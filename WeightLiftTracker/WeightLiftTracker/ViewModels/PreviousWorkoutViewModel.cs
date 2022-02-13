fb381f618cb389217589bff0ab324f1430728d1d
dnc9544 <dnc9544@gmail.com>
2022-02-12 19:54:53 -0700

    PreviousWorkout page now displays workout info and is non editable

--- WeightLiftTracker/WeightLiftTracker/ViewModels/PreviousWorkoutViewModel.cs
+++ WeightLiftTracker/WeightLiftTracker/ViewModels/PreviousWorkoutViewModel.cs
@@ -27,69 +27,43 @@ namespace WeightLiftTracker.ViewModels
             StartTime = Workout.StartTime.TimeOfDay;
             EndTime = Workout.EndTime.TimeOfDay;
             Date = Workout.StartTime.Date;
-
-            //Title = Worko;
-            IsBusy = true;
+            Title = Date.ToString("MM/dd/yyyy") + " - " + Workout.RoutineName;
+            await ExecuteLoadItemsCommand();
         }
 
         public Command LoadExercisesCommand { get; }
-        public Command AddExerciseCommand { get; }
-        public Command<WorkoutExercise> AddSetCommand { get; }
-        public Command FinishWorkoutCommand { get; }
-        public Command<WorkoutExercise> DeleteExerciseCommand { get; }
         public PreviousWorkoutViewModel()
         {
             Exercises = new ObservableCollection<WorkoutExercise>();
             LoadExercisesCommand = new Command(async () => await ExecuteLoadItemsCommand());
-            DeleteExerciseCommand = new Command<WorkoutExercise>(DeleteExercise);
-            AddSetCommand = new Command<WorkoutExercise>(AddSet);
-
-            AddExerciseCommand = new Command(OnAddItem);
-            FinishWorkoutCommand = new Command(FinishWorkout);
         }
         public async Task ExecuteLoadItemsCommand()
         {
-            IsBusy = true;
             if (Workout == null)
             {
-                IsBusy = false;
                 return;
             }
 
             try
             {
-                if (Exercises == null || Exercises.Count == 0)
+                if (Exercises.Count == 0)
                 {
                     var sets = await App.Database.GetSetsByWorkout(Workout.Id);
-                    sets.Select(s => s.ExerciseId);
-                    Exercises = new ObservableCollection<WorkoutExercise>();
-                    foreach (var set in sets)
+                    var exercises = sets.GroupBy(s => s.ExerciseId);
+                    foreach (var ex in exercises)
                     {
-                        var exercise = Exercises.FirstOrDefault(e => e.ExerciseId == set.ExerciseId);
-                        if (exercise != null)
+                        var workoutSets = new ObservableCollection<WorkoutSet>();
+                        foreach (var set in ex)
                         {
-                            exercise.Add(
-                                new WorkoutSet(exercise.ExerciseName)
-                                {
-                                    Id = exercise.Count,
-                                    Weight = set.Weight,
-                                    Reps = set.Reps
-                                }
-                            );
-                        }
-                        else
-                        {
-                            ObservableCollection<WorkoutSet> ws = new ObservableCollection<WorkoutSet>
+                            workoutSets.Add(new WorkoutSet(set.ExerciseName)
                             {
-                                new WorkoutSet(exercise.ExerciseName)
-                                {
-                                    Weight = set.Weight,
-                                    Reps = set.Reps
-                                }
-                            };
-                            WorkoutExercise we = new WorkoutExercise(set.ExerciseId, set.ExerciseName, ws);
-                            Exercises.Add(we);
+                                Id = set.Id,
+                                ExerciseName = set.ExerciseName,
+                                Reps = set.Reps,
+                                Weight = set.Weight
+                            });
                         }
+                        Exercises.Add(new WorkoutExercise(ex.Key,ex.FirstOrDefault().ExerciseName, workoutSets));
                     }
                 }
             }
@@ -97,95 +71,6 @@ namespace WeightLiftTracker.ViewModels
             {
                 Debug.WriteLine(ex);
             }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
-        public void DeleteExercise(WorkoutExercise exercise)
-        {
-            if (exercise == null)
-                return;
-
-            IsBusy = true;
-
-            try
-            {
-                Exercises.Remove(exercise);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
-        public void AddSet(WorkoutExercise exercise)
-        {
-            if (exercise == null && exercise.ExerciseId == 1)
-                return;
-
-            try
-            {
-                Exercises.FirstOrDefault(x => x.ExerciseId == exercise.ExerciseId).Add(new WorkoutSet(exercise.ExerciseName) { Id = exercise.Count });
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-        }
-
-        public void OnAppearing()
-        {
-            IsBusy = true;
-        }
-
-        private async void OnAddItem(object obj)
-        {
-            //await Shell.Current.GoToAsync($"{nameof(AddExerciseToRoutine)}?routineId={Routine.Id}");
-        }
-
-        public async void FinishWorkout()
-        {
-            //if (EndTime == null)
-            //{
-            //    EndTime = DateTime.Now.TimeOfDay;
-            //}
-            //Workout workout = new Workout
-            //{
-            //    StartTime = Date.Date + StartTime,
-            //    EndTime = Date.Date + EndTime.GetValueOrDefault(),
-            //    RoutineId = Routine.Id,
-            //    RoutineName = Routine.Name,
-            //    Duration = EndTime.GetValueOrDefault() - StartTime
-            //};
-            //int rows = await App.Database.SaveWorkoutAsync(workout);
-            //if (rows > 0)
-            //{
-            //    foreach (var ex in Exercises)
-            //    {
-            //        for (int i = 0; i < ex.Count; i++)
-            //        {
-            //            if (ex[i].Reps > 0)
-            //            {
-            //                Set set = new Set
-            //                {
-            //                    SetNumber = i,
-            //                    ExerciseId = ex.ExerciseId,
-            //                    ExerciseName = ex.ExerciseName,
-            //                    Reps = ex[i].Reps,
-            //                    Weight = ex[i].Weight,
-            //                    WorkoutId = workout.Id
-            //                };
-            //                rows = await App.Database.SaveSetAsync(set);
-            //            }
-            //        }
-            //    }
-            //}
-
-            //await Shell.Current.GoToAsync("..");
         }
     }
 }
